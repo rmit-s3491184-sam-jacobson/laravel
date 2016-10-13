b62ff0c829700ff53ad54f6240e097e151e5fd1e
lucasdobsonRMIT <s3449291@student.rmit.edu.au>
2016-10-14 01:18:37 +1100

    updated NodeJS project

--- Assign2-B/controllers/index.js
+++ Assign2-B/controllers/index.js
@@ -2,9 +2,14 @@ var express = require('express');
 var passport = require('passport');
 var router = express.Router();
 var mongoose = require('mongoose');
+var request = require('request');
+var Comment = require('../models/comment');
+
 
 //to have access to credentials.js
 var credentials = require('../config/credentials.js');
+
+var requestUrl = 'https://api.themoviedb.org/3/discover/movie?api_key=3ef0081c4257264909dd561eda211666&language=en-US&sort_by=release_date.desc&year=2016';
 
 
 var opts = {
@@ -16,6 +21,22 @@ var opts = {
 // reading credentials from credentials.js
 mongoose.connect(credentials.mongo.development.connectionString, opts);
 
+router.get('/test', function (req, res) {
+  request(requestUrl, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+
+      // parse the json result
+      var result = JSON.parse(body);
+      console.log(result);
+      //res.writeHead(200, {'Content-Type': 'application/json'});
+     // res.write(JSON.stringify(result));
+     res.render('test.hbs', { movie: JSON.stringify(result), title: 'Movie Blog' });
+    } else {
+      console.log(error, response.statusCode, body);
+    }
+
+  });
+});
 
 
 
@@ -43,6 +64,58 @@ router.get('/signup', function(req, res) {
 router.get('/profile', isLoggedIn, function(req, res) {
   res.render('profile.hbs', { user: req.user });
 });
+
+router.get('/get-data', function(req, res, next) {
+  Comment.find()
+      .then(function(doc) {
+          res.render('test.hbs', {movie: doc});
+      });
+});
+
+//UPDATE
+router.get('/update', function(req, res, next) {
+ //var objid = new require('mongodb').ObjectID("57ff837c39bc3613f03961c7");
+
+  company = Comment.findById("57ff837c39bc3613f03961c7", function(err, doc) {
+    if (err) {
+      console.error('error, no entry found');
+    }
+    res.render('test.hbs', { movie: doc, title: 'Movie Blog' });
+  });
+  //var result = JSON.parse(company);
+  //res.render('test.hbs', { movie:JSON.stringify(result) , title: 'Movie Blog' });
+  //var result = JSON.parse(company);
+
+
+  /*UserData.findById(id, function(err, doc) {
+    if (err) {
+      console.error('error, no entry found');
+    }
+    doc.title = req.body.title;
+    doc.content = req.body.content;
+    doc.author = req.body.author;
+    doc.save();
+  })*/
+
+});
+
+//CREATE
+router.get('/insert', function(req, res, next) {
+
+
+  var data = new Comment({
+    comment: {
+      name: "Lucas",
+      message: "this is a message",
+      replies: [{name: "Rick", message: "Reply 1"}, {name: "Vin", message: "Reply 2"}, {name: "Luke", message: "Reply 3"}]
+
+    }
+  });
+  data.save();
+
+  res.redirect('/');
+});
+
 
 router.get('/logout', function(req, res) {
   req.logout();
